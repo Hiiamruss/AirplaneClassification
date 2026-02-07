3bfb56d1d1714459eb5007a7bde536b2599f0975
Russell <russellsebastiansusanto@gmail.com>
2026-02-07 13:10:46 +0700

    initial commit

--- streamlit.py
+++ streamlit.py
@@ -24,16 +24,17 @@ breed_model = load_learner("plane_model_fastai284.pkl")
 
 def predict(uploaded_file):
     real_img = PILImage.create(uploaded_file)
-    resized_img = real_img.resize((28, 28), Image.NEAREST)
+    resized_img = real_img.resize((224, 224), Image.NEAREST)
     pred_class, pred_idx, outputs = breed_model.predict(resized_img)
-    return pred_class
+    label = f"{pred_class} - {outputs[pred_idx]*100:.2f}%"
+    return label
 
 uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    st.image(uploaded_file, caption="Uploaded Image")
 
     prediction = predict(uploaded_file)
-    st.subheader(f"Predicted Breed: {prediction}")
+    st.subheader(f"Predicted Aircraft: {prediction}")
 
 st.text("Built with Streamlit and FastAI, Made by russell.")
--- streamlit_io.py
+++ streamlit_io.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from fastai.vision.all import *
+import pathlib
+
 
 
 st.title("Airplane Identifier")
@@ -21,16 +23,17 @@ breed_model = load_learner("plane_model_fastai284.pkl")
 
 def predict(uploaded_file):
     real_img = PILImage.create(uploaded_file)
-    resized_img = real_img.resize((28, 28), Image.NEAREST)
+    resized_img = real_img.resize((224, 224), Image.NEAREST)
     pred_class, pred_idx, outputs = breed_model.predict(resized_img)
-    return pred_class
+    label = f"{pred_class} - {outputs[pred_idx]*100:.2f}%"
+    return label
 
 uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    st.image(uploaded_file, caption="Uploaded Image")
 
     prediction = predict(uploaded_file)
-    st.subheader(f"Predicted Breed: {prediction}")
+    st.subheader(f"Predicted Aircraft: {prediction}")
 
 st.text("Built with Streamlit and FastAI, Made by russell.")
